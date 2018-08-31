6f53f8ea9cf3350bedf9d81e93ab9c97d5f22ca1
chalove <loveun1988@outlook.com>
2018-08-31 11:15:19 +0800

    # fixed auth layout css

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -20,7 +20,10 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">
     <link rel="shortcut icon" href="images/favicon.ico">
     <!-- Styles -->
-    <link href="{{ asset('public/css/app.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('public/css/app.css') }}" rel="stylesheet"> -->
+    <style>
+
+    </style>
 
 
 </head>
